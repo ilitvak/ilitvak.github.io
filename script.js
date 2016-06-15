c081b82a736c237a540162f81c2b57d67cf03bce
Irwin Litvak <irwin.litvak@gmail.com>
2016-06-15 12:36:27 -0400

    fixed structuring of skill area

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ $(".text-fields input").click(function(){
    $(this).next().hide();
 });
 
+$(".text-fields input").focusin(function(){
+    $(this).click();
+});
+
 $(".text-fields input").blur(function(){
     if($(this).val() == "") {
         $(this).next().show();
