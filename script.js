70439df889bd4b897c0045068d269851c270d749
Irwin Litvak <irwin.litvak@gmail.com>
2016-06-28 16:58:55 -0400

    made changes to index file and made more media queries for screen size 640px

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 
+
+
 /*Hides input text when clicked on*/
 
 $(".text-fields input, .text-fields textarea").click(function(){
@@ -14,4 +16,9 @@ $(".text-fields input, .text-fields textarea").blur(function(){
         $(this).next().show();
     }
 });
+
+var randomColor = Math.floor(Math.random() * 256);
+
+
+$(".social-icons-container::before").css("color", randomColor); 
 
